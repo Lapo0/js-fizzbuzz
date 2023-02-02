84d02e406aa949b07f5c6f33fcd174161b8fc379
Lapo0 <lapo.gini.01@gmail.com>
2023-02-02 18:11:23 +0100

    RAFACTOR for

--- js/app.js
+++ js/app.js
@@ -1,17 +1,14 @@
 // Da 1 a 100
 
 for (let i = 1; i <= 100; i++) {
-    console.log(i)
+    let output = '';
 
-    const resto = i % 15
-   // console.log('resto')
-
-    if (resto === 0) {
-        console.log('FizzBuzz')
-    } else if (resto === 5 || resto === 10) {
-        console.log('Buzz')
-    } else if (resto === 3 || resto === 6 || resto === 9 || resto === 12) {
-        console.log('Fizz')
+    if (i % 3 === 0) {
+        output += 'Fizz';
+    }
+    if (i % 5 === 0) {
+        output += 'Buzz';
     }
 
+    console.log(output || i);
 }
