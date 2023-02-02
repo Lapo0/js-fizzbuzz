85741dd191a51aaf38f16437d8b8684f6b4636c7
Lapo0 <lapo.gini.01@gmail.com>
2023-02-02 14:40:42 +0100

    REFACTOR console.log in for

--- js/app.js
+++ js/app.js
@@ -7,11 +7,11 @@ for (let i = 1; i <= 100; i++) {
    // console.log('resto')
 
     if (resto === 0) {
-        console.log('resto x 15')
+        console.log('FizzBuzz')
     } else if (resto === 5 || resto === 10) {
-        console.log('resto x 5')
+        console.log('Buzz')
     } else if (resto === 3 || resto === 6 || resto === 9 || resto === 12) {
-        console.log('resto x 3')
+        console.log('Fizz')
     }
 
 }
